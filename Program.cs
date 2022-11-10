2ad511c9ffb6163728d7512585a3e2f06a1ba0df
Edvin Larsson <edvinpontuslarsson@gmail.com>
2022-11-10 18:19:56 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MySkills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] skills = {
+                "Postman",
+                "Jest",
+                "JUnit",
+                "Git",
+                "Docker",
+                "MongoDB",
+                "SQL",
+                ".NET",
+                "Node.js",
+                "React",
+                "CSS",
+                "HTML",
+                "YAML",
+                "Linux",
+                "REST",
+                "Test-driven development",
+                "Integrationstestning",
+                "Scrum",
+                "Continuous Integration"
+            };
+
+            Array.Sort(skills, (a, b) => String.Compare(a, b));
+
+            Console.WriteLine(String.Join(", ", skills));
+        }
+    }
+}
